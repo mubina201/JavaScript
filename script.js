4cc6388b36885e7f559403df017a4c77bcb7e528
mubina201 <sabirovamubina15@gmail.com>
2024-11-09 02:01:57 +0500

    now

---
+++ script.js
@@ -0,0 +1,16 @@
+// Data types - Ma'lumot turlari - 8 tur
+// O'zgaruvchi va o'zgarmaslar
+// var , let        const
+'' , "" , ``
+const name=prompt("Ismingiz nima?");
+const surname=prompt("Familiyangiz nima");
+const year=prompt("Nechanchi yil tavallud topgansiz?");
+const date=prompt("Nechanchi sanada va qaysi oyda tavallud topgansiz?");
+const where=prompt("Qayerda tug'ilgansiz?");
+const yosh=prompt("Hozirda necha yoshsiz?");
+const which=prompt("Nechanchi sinf o'quvchisisiz?");
+const school=prompt("Qaysi maktabda tahsil olasiz?");
+const stay=prompt("Hozirda qayerda yashab kelayabsiz?");
+const tutor=prompt("Qaysi qo'shimcha darslarga qatnaysiz yoki qiziqgan faningiz qaysi?");
+const job=prompt("Kelajakda kim bo'lishni xohlaysiz?");
+console.log(`Assalomu laykum! Mening ismim ${name}. Familiyam ${surname}. Men ${year}-yil ${date}da ${where} tavallud topganman. Hozirda ${yosh} yoshdaman va ${which}-sinf o'quvchisiman. Men ${school}-umumiy o'rta ta'lim maktabida tahsil olaman. Hozirda ${stay}da yashab kelyapman. Maktab darslaridan tashqari boshqa fanlarga ham qiziqaman. Misol uchun ${tutor} yo'nalishi. Men bu fanlarimda qo'shimcha darslar olaman. Kelajakda ${job} bo'lmoqchiman va buning uchun judayam katta harakat qilyapman.`);
